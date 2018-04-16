8acd94fcf107d14f9920cfc310e2090e16c25316
nirav2770 <npatel@Niravs-MacBook-Pro.local>
2018-04-16 16:37:39 -0400

    added TestProject project

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        for(int i=1;i<=10;i++){
+        	System.out.println("Hello World ..." + i);
+        }
+        
+    }
+
+}
